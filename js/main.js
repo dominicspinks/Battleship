6ebe432590d53204c6cd3d2ff5903910877a2657
Dominic Spinks <dominic.spinks@outlook.com>
2023-10-21 00:01:22 +1100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Stage 1: Player places their pieces
+// Initialise board
+// Player will hover mouse over the board and it will highlight the cells the boat will go in
+// - for a size 5 boat 5 cells will be highlight
+// Player can right click to rotate the boat
+// Player clicks to place the boat on the board
+// The boat is highlighted to show it is placed (along the bottom)
+// The next boat will be highlighted
+// Player repeats until all boats are placed
+
+// Stage 2: guessing
+// Initialise boards (player and computer)
+// - The boats on the player's board will be shown as dots
+// Randomise which player goes first
+// When player picks, the click on the board the cell they want to pick
+// - validate if the cell has been selected already
+// if it is a hit, highlight red
+// if it is a miss, highlight blue
+// if the boat is sunk, highlight grey
