c7df20ae8dc2ebaa638e3798ed298d42cf2387ad
Dominic Spinks <dominic.spinks@outlook.com>
2023-10-21 13:37:26 +1100

    Add basic game grid and js pseudocode

--- js/main.js
+++ js/main.js
@@ -8,12 +8,46 @@
 // The next boat will be highlighted
 // Player repeats until all boats are placed
 
+//// Board Data:
+// Number to indicate which boat is placed (0=nothing in the cell)
+// +/- to define if the cell has been selected by the opponent
+
+// Initialise the computer board
+// The 'AI' picks its positions for each boat:
+// Pick a random orientation of the boat (horizontal/vertical)
+// Pick a random cell
+// - Validate if the boat can fit (has enough vertical/horizontal space)
+//    - This will check for other boats already placed and the edges of the board
+// Place the next boat until all boats are placed
+
 // Stage 2: guessing
-// Initialise boards (player and computer)
-// - The boats on the player's board will be shown as dots
+// - The boats on the player's board will be shown as dots, but hidden on the computer's board
 // Randomise which player goes first
-// When player picks, the click on the board the cell they want to pick
+// When player picks (on the computer board), they click on the board the cell they want to pick
+// - Only allow selecting on the computer board (hover/click on the player board will do nothing)
 // - validate if the cell has been selected already
-// if it is a hit, highlight red
-// if it is a miss, highlight blue
-// if the boat is sunk, highlight grey
+// - if it is a hit, highlight red
+// - if it is a miss, highlight blue
+// - if the boat is sunk, highlight grey
+
+// Computer picks, highlights on the player board
+// - Same highlighting rules as above
+
+// After a boat is hit, check if that boat is sunk
+// After a boat is sunk, check if the other player has any boats left.
+// If yes, continue with the game
+// If no, the current player has won
+
+//// Computer cell selection:
+// If the last cell placed was a miss, pick a random cell
+// - validate the cell has not been selected already
+// If the last cell placed was a hit and sink, pick a random cell
+// - validate the cell has not been selected already
+// If the last cell placed was a hit but not a sink:
+// - Pick a cell next to the last placed cell
+//   - If there is a row of cells already hit, pick the next cell in the row
+//   - Need to handle cases where two boats are next to each other (inline or perpendicular)
+
+/** Extension:
+ * Add option to play with 'Salvos' rules
+ */
